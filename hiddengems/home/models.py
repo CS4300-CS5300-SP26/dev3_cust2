afeac927df82fddb4f43ba49ee4da0c6084ce191
Royer5000 <royerjr21@gmail.com>
2026-03-17 17:57:36 +0000

    Have Purchase button that links to steam store page, view via game/steam_game_id in app

--- hiddengems/home/models.py
+++ hiddengems/home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Game(models.Model):
+    #Steam Integration
+    storefront = models.CharField(max_length=50, default="steam")
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    game_id = models.IntegerField()
--- hiddengems/home/views.py
+++ hiddengems/home/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Game
+
 
 def index(request):
     return render(request, 'index.html')
+
+
+
+def purchase_game(request, game_id):
+    game = get_object_or_404(Game, game_id=game_id)
+
+    return render(request, "purchase_game.html", {
+        "storefront" : game.storefront,
+        "price" : game.price,
+        "game_id" : game.game_id,
+    })
