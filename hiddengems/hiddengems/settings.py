444f9b5541b72a3045289f0d58c455daf20e859f
efutey15 <ejf.colorado@gmail.com>
2026-03-18 23:58:45 +0000

    Testing with sqlite

--- hiddengems/hiddengems/settings.py
+++ hiddengems/hiddengems/settings.py
@@ -82,24 +82,13 @@ WSGI_APPLICATION = 'hiddengems.wsgi.application'
 
 USE_POSTGRES = os.getenv("USE_POSTGRES", "False") == "True"
 
-if USE_POSTGRES:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": os.getenv("POSTGRES_DB", "hiddengems"),
-            "USER": os.getenv("POSTGRES_USER", "admin"),
-            "PASSWORD": os.getenv("POSTGRES_PASSWORD", "AdminPassword1"),
-            "HOST": os.getenv("POSTGRES_HOST", "localhost"),
-            "PORT": os.getenv("POSTGRES_PORT", "5432"),
-        }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+}
 
 
 # Password validation
